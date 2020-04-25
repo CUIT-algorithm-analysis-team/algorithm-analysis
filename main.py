8f91836e438c44ba9b01487fbf04f5dd19f2945b
曾欣科 <1822042799@qq.com>
2020-04-25 20:11:20 +0800

    添加了一些TODO

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from drawing import *
 from sort_algorithm import *
 from statistic import *
 
+import TKinter
+top = TKinter.Tk()
+top.mainloop()
+
 if __name__ == '__main__':
-    a = algorithm_analysis(sorted_algorithms)
-    a.test_time(1000)
-    drawCostTime(a.costed_time,1000)
+    top = TKinter.Tk()
+    top.mainloop()
+    # a = algorithm_analysis(sorted_algorithms)
+    # a.test_time(1000)
+    # drawCostTime(a.costed_time,1000)
