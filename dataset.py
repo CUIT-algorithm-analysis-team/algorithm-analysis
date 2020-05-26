25dd90e4c576b5314f6cdf5c48c8419714eede77
yanzhe <zhe.yan2015@outlook.com>
2020-05-27 00:39:39 +0800

    modify load_data() func
    add param start and step

--- dataset.py
+++ dataset.py
@@ -7,13 +7,15 @@ class dataset():
         """
         self.datapath = "data"
     @staticmethod
-    def load_data(n):
+    def load_data(n, start=1, step=1):
         '''
         直接把乱序数据输出去
         :param n: n 数据规模
+        :param start: 起始规模
+        :param step: 步长
         :return:
         '''
-        for i in range(1,n + 1):
+        for i in range(start, n+1, step):
             l = [j for j in range(i)]
             shuffle(l)
             yield l
@@ -41,7 +43,7 @@ class dataset():
             yield l
 
 if __name__ == '__main__':
-    for l in dataset.load_data(1000):
+    for l in dataset.load_data(4000, 100, 100):
         print(l)
     print("finish")
 
---
+++ regression_alys.py
@@ -0,0 +1,68 @@
+import numpy as np
+from scipy.optimize import least_squares
+
+
+def residuals_nlogn(param, y, x):
+    a, b = param
+    return y - (a * x * np.log(x) + b)
+
+
+def residuals_n2(param, y, x):
+    a, b = param
+    return y - (a * np.power(x, 2) + b)
+
+
+def regression(lst_time, lst_size):
+    """
+    Regression Analysis.
+    :param lst_time: A list, cost time as Y axis.
+    :param lst_size: A list, question size as X axis
+    :return: A dict.{'type': str 'nlogn' or 'n2',
+                     'cost': float,
+                     'pred': list,
+                     'param': tuple(a, b)  nlogn is (a*nlogn + b). n2 is  (a*n^2 + b)}
+    """
+    out = {}
+    nlogn = least_squares(residuals_nlogn, [1, 1], args=(np.array(lst_time), np.array(lst_size)))
+    n2 = least_squares(residuals_n2, [1, 1], args=(np.array(lst_time), np.array(lst_size)))
+
+    if nlogn['cost'] < n2['cost']:
+        pred_time_nlogn = nlogn['x'][0] * np.array(lst_size) * np.log(lst_size) + nlogn['x'][1]
+        out['type'] = 'nlogn'
+        out['cost'] = round(nlogn['cost'], 6)
+        out['pred'] = pred_time_nlogn.tolist()
+        out['param'] = (nlogn['x'][0], nlogn['x'][1])
+    else:
+        pred_time_n2 = n2['x'][0] * np.power(np.array(lst_size), 2) + n2['x'][1]
+        out['type'] = 'n2'
+        out['cost'] = round(n2['cost'], 6)
+        out['pred'] = pred_time_n2.tolist()
+        out['param'] = (n2['x'][0], n2['x'][1])
+
+    return out
+
+if __name__ == '__main__':
+    from dataset import dataset
+    from sort_algorithm import *
+    import time
+    import matplotlib.pyplot as plt
+
+    data = dataset()
+    lst_size = []
+    lst_time = []
+
+    for item in data.load_data(1000, 100, 100):
+        start = time.time()
+        bubble_sort(item)
+        stop = time.time()
+        lst_size.append(len(item))
+        lst_time.append((stop-start))
+
+    out = regression(lst_time, lst_size)
+    plt.figure()
+    plt.plot(lst_size, lst_time, color='red')
+    plt.plot(lst_size, out['pred'], color='blue')
+    plt.legend(["GroundTurth", out['type']])
+    plt.show()
+    print(f"Cost is {out['cost']}\nParam is {out['param']}")
+
